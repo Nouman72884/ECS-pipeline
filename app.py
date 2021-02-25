53855d697604a67929749b53aa9dad3184dc0063
Umair841 <umair.younas@eurustechnologies.com>
2021-02-25 20:47:17 +0500

    first commit to repo

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Say hello to Flask !--! images are fetched from ECR !--! nginx is working as a reverse proxy for our app'
+
+app.run(host='0.0.0.0', port=5000)
+
