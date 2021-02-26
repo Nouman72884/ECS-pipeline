26bc11286915ff83c51b0c0236788afe5bfc7d23
Umair841 <77967477+Umair841@users.noreply.github.com>
2021-02-26 19:56:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Say hello to Flask !--! images are fetched from ECR !-kar dia change-! nginx is working as a reverse proxy for our app'
+    return 'Say hello to Flask !--! images are fetched from ECR !-2nd tym chaanged-! nginx is working as a reverse proxy for our app'
 
 app.run(host='0.0.0.0', port=5000)
 
