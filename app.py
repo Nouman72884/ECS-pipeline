a03fa661fa085e009ed0757a20c12a8575caa3fa
Umair841 <77967477+Umair841@users.noreply.github.com>
2021-03-02 21:04:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Say hello to Flask !--! now pipeline is aslo deployed by script >> testing for 6th time with final stack<<-! nginx is working as a reverse proxy for our app'
+    return 'Say hello to Flask !--! now pipeline is aslo deployed by script >> testing change in code<<-! nginx is working as a reverse proxy for our app'
 
 app.run(host='0.0.0.0', port=5000)
 
