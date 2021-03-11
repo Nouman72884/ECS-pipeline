453bf5764060ce0d99e4602dd539d418142cb17c
Umair841 <77967477+Umair841@users.noreply.github.com>
2021-03-11 20:29:03 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Say hello to Flask !--! ok so now this is automated with jenkins OpXxXx Finally did that "next try" <<-! nginx is working as a reverse proxy for our app'
+    return 'Say hello to Flask !--! outomation with jenkins is done now <<-! nginx is working as a reverse proxy for our app'
 
 app.run(host='0.0.0.0', port=5000)
 
