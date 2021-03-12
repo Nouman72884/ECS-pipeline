ec4254e8d373c3e2d5f1bca4aeba77b6e8109054
Umair841 <77967477+Umair841@users.noreply.github.com>
2021-03-12 15:19:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Say hello to Flask !--! outomation with jenkins is done now <<-! nginx is working as a reverse proxy for our app on ECS with V5'
+    return 'Say hello to Flask !--! outomation with jenkins is done now <<-! nginx is working as a reverse proxy for our app on ECS with V5 /n Umair'
 
 app.run(host='0.0.0.0', port=5000)
 
